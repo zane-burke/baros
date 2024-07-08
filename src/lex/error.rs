dc4798bd386ac0dae072fc9b75c48bc19dc3eb7d
Zane Burke <zaneburke418@gmail.com>
2024-07-08 01:13:03 -0400

    added some comments to the variants of LexErrorType

--- src/lex/error.rs
+++ src/lex/error.rs
@@ -30,15 +30,16 @@ impl Span {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum LexErrorType {
-    StringEscape, // unescaped slash
-    TrailingUnderscore, // 
-    OutOfRadixBounds,
-    NoIntValue,
-    UnicodeEscape(UnicodeEscapeError),
-    UnterminatedString,
-    UnrecognizedToken { tok: char },
-    DisallowedToken { tok: char },
-    BadIdentifier { name: EcoString },
+    StringEscape,                      // unescaped slash
+    TrailingUnderscore,                // 100_
+    OutOfRadixBounds,                  // 0b12
+    NoIntValue,                        // 0x
+    UnicodeEscape(UnicodeEscapeError), // \u{-12} (idk)
+    UnterminatedString,                // " What the heck! This string isn't terminated!
+    UnterminatedComment,               // /* What the heck! This comment isn't ever terminated!
+    UnrecognizedToken { tok: char },   // idk
+    DisallowedToken { tok: char },     // ; <- we hate these
+    BadIdentifier { name: EcoString }, // idk
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
